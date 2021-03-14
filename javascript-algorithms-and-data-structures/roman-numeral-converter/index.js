33cbd70811aac6f312f8c9977c2989bdc8cb84d6
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-03-14 13:35:26 +0000

    update

--- javascript-algorithms-and-data-structures/roman-numeral-converter/index.js
+++ javascript-algorithms-and-data-structures/roman-numeral-converter/index.js
@@ -1,15 +1,21 @@
+/*
+ *    Author: Samuele Girgenti
+ *    Date: 14 / 03 / 2021
+ */
+
+//convert to roman number. Recursive approach
 function convertToRoman(num) {
   const nums = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
   const rnums = [ "I", "IV", "V", 
                   "IX", "X", "XL",
                   "L", "XC", "C", 
                   "CD", "D", "CM", "M" ];
-
   let i = 0;
-  let result;
 
+  //if num is zero return nothing
   if (num == 0) return "";
 
+  //searching for the correct digit
   while (nums[i] <= num) {
     ++i;
   }
@@ -18,4 +24,3 @@ function convertToRoman(num) {
   return rnums[i - 1] + convertToRoman(num - nums[i - 1]);
 }
 
-//for (let i = 220; i < 239; ++i) console.log(i, convertToRoman(i));
