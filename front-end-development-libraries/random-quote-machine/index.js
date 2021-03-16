db5d5d4c60f3d3fb21d8ecee63a941fcdb2449d7
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-03-16 17:26:20 +0100

    update

--- front-end-development-libraries/random-quote-machine/index.js
+++ front-end-development-libraries/random-quote-machine/index.js
@@ -16,12 +16,46 @@ function getRandomQuote(handleQuote) {
   return quote.keys;
 }
 
+//show on page a random quote
+function showRandomQuote() {
+  getRandomQuote((quote) => {
+    $("#text").text(quote.content);
+    $("#author").text(quote.author);
+  });
+}
+
+function setPageColor(color) {
+  $("body").css("background-color", color);
+  $("#quote-box").css("color", color);
+}
+
+function getRandomColor() {
+  const colors = [
+    "#4287f5",
+    "#ff3021",
+    "#ffd500",
+    "#73ff00",
+    "#00e5ff",
+    "#b300ff",
+    "#ff00d0",
+    "#9d25d9",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * (colors.length - 1));
+
+  return colors[randomIndex];
+}
+
 //execute when page il fully loaded
 $(document).ready(() => {
-  let quote;
-  getRandomQuote((data) => {
-    quote = data;
-    console.log(quote);
+  //load first quote
+  showRandomQuote();
+  setPageColor(getRandomColor());
+
+  $("#new-quote").click(() => {
+    //set a new random quote
+    showRandomQuote();
+
+    setPageColor(getRandomColor());
   });
-  console.log(quote);
 });
