eaccc7e2c311692728015f19e08fcdc62ae1acf6
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-03-17 11:40:24 +0100

    update

--- front-end-development-libraries/markdown-previewer/index.js
+++ front-end-development-libraries/markdown-previewer/index.js
@@ -4,24 +4,38 @@ class Editor extends React.Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <div id="editor-box">
-        <textarea id="editor"></textarea>
+        <textarea
+          id="editor"
+          value={this.props.md}
+          onChange={this.props.onChange}
+        ></textarea>
       </div>
     );
   }
 }
 
+const renderer = new marked.Renderer();
+
 class Previewer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.mdToHtml = this.mdToHtml.bind(this);
+  }
+
+  mdToHtml(mdSource) {
+    return { __html: marked(mdSource, { renderer: renderer }) };
   }
 
   render() {
     return (
-      <div id="preview">
-        <p>previewer</p>
-      </div>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={this.mdToHtml(this.props.md)}
+      ></div>
     );
   }
 }
@@ -29,13 +43,26 @@ class Previewer extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      mdSource: "test",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      mdSource: event.target.value,
+    });
   }
 
   render() {
     return (
       <div id="app-box">
-        <Editor />
-        <Previewer />
+        <Editor md={this.state.mdSource} onChange={this.handleChange} />
+        <Previewer md={this.state.mdSource} />
       </div>
     );
   }
