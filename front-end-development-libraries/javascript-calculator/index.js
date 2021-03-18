ac2f15816b07e8893441ead0a0baa0408e29ce21
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-03-18 11:48:11 +0100

    init Javascript Calculator

---
+++ front-end-development-libraries/javascript-calculator/index.js
@@ -0,0 +1,17 @@
+//Calculator component
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+//render calculator
+ReactDOM.render(<Calculator />, document.getElementById("app"));
