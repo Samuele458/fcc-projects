c5564111c773f6cbe3d73d649fb9c5a8a29b3db6
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-03-22 19:10:51 +0100

    Bar Chart

--- data-visualization/bar-chart/index.js
+++ data-visualization/bar-chart/index.js
@@ -1,4 +1,9 @@
-let width = 600;
+/*
+ *  Author: Samuele Girgenti
+ *  Date: 22 / 03 / 2021
+ */
+
+let width = 700;
 let height = 350;
 let padding = 50;
 
@@ -25,6 +30,27 @@ d3.json(
       .range([padding, width - padding]);
     console.log(heightScale);
 
+    const title = d3
+      .select(".graph")
+      .append("h1")
+      .text(data.name)
+      .attr("id", "title")
+      .style("text-align", "center");
+
+    const tooltip = d3
+      .select(".graph")
+      .append("div")
+      .attr("id", "tooltip")
+      .style("background-color", "rgb(153, 153, 255)")
+      .style("border-radius", "3px")
+      .style("box-shadow", "3px 2px 2px black")
+      .style("width", "70px")
+      .style("height", "30px")
+      .style("opacity", "0")
+      .style("position", "absolute")
+      .style("font-size", "8px")
+      .style("text-align", "center");
+
     const svg = d3
       .select(".graph")
       .append("svg")
@@ -36,7 +62,6 @@ d3.json(
       .data(dataset)
       .enter()
       .append("rect")
-      .style("fill", "red")
       .attr("width", colWidth)
       .attr("height", (d) => {
         return height - heightScale(d[1]) - padding;
@@ -46,7 +71,22 @@ d3.json(
       })
       .attr("x", (d, i) => {
         return padding + i * (colWidth + colPadding);
+      })
+      .attr("data-date", (d) => d[0])
+      .attr("data-gdp", (d) => d[1])
+      .attr("class", "bar")
+      .on("mouseover", (d, i) => {
+        tooltip
+          .style("top", `${height}px`)
+          .style("left", `${i * colWidth + padding}px`)
+          .style("opacity", "100")
+          .attr("data-date", d[0])
+          .html(`<p>Date: ${d[0]}</p><p>GDP: ${d[1]}</p>`);
+      })
+      .on("mouseout", (d, i) => {
+        tooltip.style("opacity", "0");
       });
+
     const xAxis = d3.axisBottom(xAxisScale);
 
     const yAxis = d3.axisLeft(heightScale);
@@ -54,10 +94,12 @@ d3.json(
     svg
       .append("g")
       .attr("transform", "translate(0," + (height - padding) + ")")
-      .call(xAxis);
+      .call(xAxis)
+      .attr("id", "x-axis");
     svg
       .append("g")
       .attr("transform", "translate(" + padding + ",0)")
-      .call(yAxis);
+      .call(yAxis)
+      .attr("id", "y-axis");
   }
 );
