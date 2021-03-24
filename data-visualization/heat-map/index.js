5d303ed0b4cdb9d34be074820666427216526ce8
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-03-24 16:38:28 +0100

    Heat Map

--- data-visualization/heat-map/index.js
+++ data-visualization/heat-map/index.js
@@ -1,3 +1,8 @@
+/*
+ *  Author: Samuele Girgenti
+ *  Date: 24 / 03 / 2021
+ */
+
 const width = 1300;
 const height = 550;
 const padding = 70;
@@ -27,6 +32,8 @@ d3.json(
     let temp = data.baseTemperature;
     const dataset = data.monthlyVariance.map((d) => {
       d.temperature = Math.round((temp + d.variance) * 10) / 10;
+      d.yearDate = new Date(Date.UTC(d.year, 0, 1, 0, 0, 0));
+
       return d;
     });
 
@@ -36,15 +43,29 @@ d3.json(
     const elementHeight = (height - padding * 2) / 12;
 
     console.log(dataset[dataset.length - 1].year - dataset[0].year);
+    console.log(elementWidth);
+    d3.select(".graph")
+      .append("h1")
+      .text("Global temperature")
+      .attr("id", "title");
+    d3.select(".graph")
+      .append("h2")
+      .text("Global temperature month-by-month, from 1753 to 2015")
+      .attr("id", "description");
 
-    d3.select(".graph").append("h1").text("Title").attr("id", "title");
-    d3.select(".graph").append("h2").text("subtitle").attr("id", "description");
-
+    /*
     let xScale = d3
       .scaleLinear()
       .domain([d3.min(dataset, (d) => d.year), d3.max(dataset, (d) => d.year)])
-      .range([padding, width - padding / 2]);
-
+      .range([padding, width - padding]);
+*/
+    let xScale = d3
+      .scaleTime()
+      .domain([
+        d3.min(dataset, (d) => d.yearDate),
+        d3.max(dataset, (d) => d.yearDate),
+      ])
+      .range([padding, width - padding]);
     let yScale = d3
       .scaleBand()
       .domain([
@@ -78,6 +99,20 @@ d3.json(
       d3.max(dataset, (d) => d.temperature),
     ]);
 
+    const tooltip = d3
+      .select(".graph")
+      .append("div")
+      .attr("id", "tooltip")
+      .style("background-color", "rgb(153, 153, 255)")
+      .style("border-radius", "3px")
+      .style("box-shadow", "3px 2px 2px black")
+      .style("width", "120px")
+      .style("height", "30px")
+      .style("opacity", "0")
+      .style("position", "absolute")
+      .style("font-size", "8px")
+      .style("text-align", "center");
+
     const svg = d3
       .select(".graph")
       .append("svg")
@@ -98,11 +133,29 @@ d3.json(
         return padding + (i % 12) * elementHeight;
       })
       .attr("class", "cell")
+      .attr("data-month", (d) => d.month - 1)
+      .attr("data-year", (d) => d.year)
+      .attr("data-temp", (d) => d.temperature)
       .style("fill", (d, i) => {
         return colorScale(d.temperature);
+      })
+      .on("mouseover", (d, i) => {
+        tooltip
+          .style("top", `${padding * 2 + (i % 12) * elementHeight}px`)
+          .style("left", `${padding + Math.floor(i / 12) * elementWidth}px`)
+          .style("opacity", "100")
+          .attr("data-year", d.year)
+          .html(
+            `<p>Year: ${d.year}</p><p>Month: ${
+              d.month - 1
+            }</p><p>Temperature: ${d.temperature}</p>`
+          );
+      })
+      .on("mouseout", (d, i) => {
+        tooltip.style("opacity", "0");
       });
 
-    const xAxis = d3.axisBottom(xScale).ticks(20).tickFormat(d3.format("1000"));
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"));
     const yAxis = d3.axisLeft(yScale);
 
     svg
@@ -129,9 +182,56 @@ d3.json(
     legend
       .append("g")
       .attr("transform", "translate(0," + (height - padding / 2.4) + ")")
-      .call(legendAxis)
-      .attr("id", "x-axis");
+      .call(legendAxis);
 
-    legend.append("rect").attr("width");
+    legend
+      .append("rect")
+      .attr("width", (width / 7 - padding) / 4)
+      .attr("height", 20)
+      .attr(
+        "transform",
+        "translate(" + padding + "," + (height - padding / 2.4 - 20) + ")"
+      )
+      .attr("fill", "blue");
+    console.log((width / 7 - padding) / 4);
+    legend
+      .append("rect")
+      .attr("width", (width / 7 - padding) / 4)
+      .attr("height", 20)
+      .attr(
+        "transform",
+        "translate(" +
+          (padding + ((width / 7 - padding) / 4) * 1) +
+          "," +
+          (height - padding / 2.4 - 20) +
+          ")"
+      )
+      .attr("fill", "white");
+    legend
+      .append("rect")
+      .attr("width", (width / 7 - padding) / 4)
+      .attr("height", 20)
+      .attr(
+        "transform",
+        "translate(" +
+          (padding + ((width / 7 - padding) / 4) * 2) +
+          "," +
+          (height - padding / 2.4 - 20) +
+          ")"
+      )
+      .attr("fill", "yellow");
+    legend
+      .append("rect")
+      .attr("width", (width / 7 - padding) / 4)
+      .attr("height", 20)
+      .attr(
+        "transform",
+        "translate(" +
+          (padding + ((width / 7 - padding) / 4) * 3) +
+          "," +
+          (height - padding / 2.4 - 20) +
+          ")"
+      )
+      .attr("fill", "red");
   }
 );
