f5164bc44e7eeb4d408edb18c2b5d2b93f94f987
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-03-25 10:29:06 +0100

    Treemap diagram

--- data-visualization/treemap-diagram/index.js
+++ data-visualization/treemap-diagram/index.js
@@ -1,3 +1,8 @@
+/*
+ *  Author: Samuele Girgenti
+ *  Date: 25 / 03 / 2021
+ */
+
 const margin = {
   top: 20,
   right: 20,
@@ -7,6 +12,24 @@ const margin = {
 
 const width = 800 + margin.left + margin.right;
 const height = 600 + margin.top + margin.bottom;
+
+const title = d3
+  .select(".graph")
+  .append("h1")
+  .text("Top 100 Most Sold Videogames")
+  .attr("id", "title");
+
+const description = d3
+  .select(".graph")
+  .append("h3")
+  .text("Top 100 Most Sold Videogames, platform-by-platform")
+  .attr("id", "description");
+
+const tooltip = d3
+  .select(".graph")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("opacity", "0");
 
 const svg = d3
   .select(".graph")
@@ -27,8 +50,15 @@ d3.json(
       .size([
         width - margin.right - margin.left,
         height - margin.top - margin.bottom,
-      ])(root);
+      ])
+      .paddingInner(1)(root);
 
+    const fader = (color) => {
+      return d3.interpolateRgb(color, "#fff")(0.3);
+    };
+    const color = d3.scaleOrdinal(d3.schemeCategory20.map(fader));
+
+    console.log(d3.schemeCategory50);
     svg
       .selectAll("rect")
       .data(root.leaves())
@@ -39,10 +69,23 @@ d3.json(
       .attr("width", (d) => d.x1 - d.x0)
       .attr("height", (d) => d.y1 - d.y0)
       .attr("class", "tile")
-      .attr("fill", "orange")
+      .attr("fill", (d) => color(d.data.category))
       .attr("data-name", (d) => d.data.name)
       .attr("data-category", (d) => d.data.category)
-      .attr("data-value", (d) => d.data.value);
+      .attr("data-value", (d) => d.data.value)
+      .on("mouseover", (d, i) => {
+        tooltip
+          .style("top", `${d3.event.pageY + 10}px`)
+          .style("left", `${d3.event.pageX + 10}px`)
+          .style("opacity", "100")
+          .attr("data-value", d.data.value)
+          .html(
+            `<p>Name: ${d.data.name}</p><p>Category: ${d.data.category}</p><p>Value: ${d.data.value}</p>`
+          );
+      })
+      .on("mouseout", (d, i) => {
+        tooltip.style("opacity", "0");
+      });
 
     svg
       .selectAll("text")
@@ -52,7 +95,6 @@ d3.json(
       .attr("class", "tile-text")
       .attr("x", (d) => d.x0 + margin.left)
       .attr("y", (d) => d.y0 + margin.top)
-      //.text("ciao ehehheh  come stai eh")
       .selectAll("tspan")
       .data((d) =>
         d.data.name.split(/\s|\//gi).map((elem) => {
@@ -68,5 +110,33 @@ d3.json(
       .attr("x", (d) => d.x + margin.left + 3)
       .attr("y", (d, i) => d.y + margin.top + 10 + i * 7)
       .text((d) => d.text);
+
+    const legendSvg = d3
+      .select(".graph")
+      .append("svg")
+      .attr("id", "legend")
+      .attr("width", width / 1.3)
+      .attr("height", height);
+
+    legendSvg
+      .selectAll("rect")
+      .data(data.children)
+      .enter()
+      .append("rect")
+      .attr("class", "legend-item")
+      .attr("height", 25)
+      .attr("width", 25)
+      .attr("x", (d, i) => margin.left + (i % 4) * 150)
+      .attr("y", (d, i) => margin.top + Math.floor(i / 4) * 50)
+      .attr("fill", (d) => color(d.name));
+
+    legendSvg
+      .selectAll("text")
+      .data(data.children)
+      .enter()
+      .append("text")
+      .attr("x", (d, i) => margin.left + (i % 4) * 150 + 35)
+      .attr("y", (d, i) => margin.top + Math.floor(i / 4) * 50 + 17)
+      .text((d) => d.name);
   }
 );
