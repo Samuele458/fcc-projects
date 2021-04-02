b78a97873ae83e35e323e481476e72d96f3b20b5
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-04-02 22:20:27 +0200

    Check Placement API

--- quality-assurance/sudoku-solver/controllers/sudoku-solver.js
+++ quality-assurance/sudoku-solver/controllers/sudoku-solver.js
@@ -1,24 +1,40 @@
 class SudokuSolver {
+  getRowPos(row) {
+    return row.charCodeAt() - 64;
+  }
 
   validate(puzzleString) {
+    return puzzleString.length == 81 && /^[\.0-9]+$/.test(puzzleString);
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    let rowLine = puzzleString.slice(9 * (column - 1), 9 * column);
+    return rowLine.indexOf(value) == -1;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    let colLine = "";
+    for (let i = 0; i < 9; ++i) {
+      colLine += puzzleString[i * 9 + (row - 1)];
+    }
+    return colLine.indexOf(value) == -1;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    let rowRegion = Math.floor((row - 1) / 3);
+    let columnRegion = Math.floor((column - 1) / 3);
+    let region = "";
 
+    for (let i = 0; i < 3; ++i) {
+      for (let j = 0; j < 3; ++j) {
+        region += puzzleString[columnRegion * 27 + i * 9 + rowRegion * 3 + j];
+      }
+    }
+
+    return region.indexOf(value) == -1;
   }
 
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- quality-assurance/sudoku-solver/routes/api.js
+++ quality-assurance/sudoku-solver/routes/api.js
@@ -1,18 +1,50 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    let checks = [
+      solver.checkRowPlacement,
+      solver.checkColPlacement,
+      solver.checkRegionPlacement,
+    ];
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    const names = ["row", "column", "region"];
 
+    checks = checks.map((checkFun) => {
+      return checkFun(
+        req.body.puzzle,
+        parseInt(req.body.coordinate[1]),
+        req.body.coordinate[0].charCodeAt() - 64,
+        parseInt(req.body.value)
+      );
     });
+
+    let conflict = names.filter((name, i) => {
+      return !checks[i];
+    });
+
+    let isValid = true;
+
+    checks.forEach((e) => {
+      if (!e) isValid = false;
+    });
+
+    let returnObj = {
+      valid: isValid,
+    };
+
+    if (conflict.length > 0) {
+      returnObj.conflict = conflict;
+    }
+
+    res.json(returnObj);
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    console.log("solve");
+  });
 };
