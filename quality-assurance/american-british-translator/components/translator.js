3a09b39eb2509fd447e94faf327cfa6475fa11b7
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-04-03 17:57:09 +0200

    update

--- quality-assurance/american-british-translator/components/translator.js
+++ quality-assurance/american-british-translator/components/translator.js
@@ -1,10 +1,87 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+
+const TO_BRITISH = "american-to-british";
+const TO_AMERICAN = "british-to-american";
 
 class Translator {
+  swap(json) {
+    var ret = {};
+    for (var key in json) {
+      ret[json[key]] = key;
+    }
+    return ret;
+  }
 
+  getRegex(locale) {
+    if (locale === TO_AMERICAN) return /^\d{1,2}\.\d{1,2}$/;
+    else if (locale === TO_BRITISH) return /^\d{1,2}\:\d{1,2}$/;
+  }
+
+  getDictionaries(locale) {
+    let dictionaries = [];
+    if (locale == TO_BRITISH) {
+      dictionaries.push(americanToBritishSpelling);
+      dictionaries.push(americanToBritishTitles);
+      dictionaries.push(americanOnly);
+    } else if (locale === TO_AMERICAN) {
+      dictionaries.push(this.swap(americanToBritishSpelling));
+      dictionaries.push(this.swap(americanToBritishTitles));
+      dictionaries.push(britishOnly);
+    }
+  }
+
+  translate(text, locale) {
+    console.log(locale);
+
+    let tokens = [];
+    let translation = "";
+    let dictionaries = [];
+    let timeRegex;
+
+    if (locale == "american-to-british") {
+      dictionaries.push(americanToBritishSpelling);
+      dictionaries.push(americanToBritishTitles);
+      dictionaries.push(americanOnly);
+      timeRegex = /^\d{1,2}\:\d{1,2}$/;
+    } else if (locale === "british-to-american") {
+      dictionaries.push(this.swap(americanToBritishSpelling));
+      dictionaries.push(this.swap(americanToBritishTitles));
+      dictionaries.push(britishOnly);
+      timeRegex = /^\d{1,2}\.\d{1,2}$/;
+    } else {
+      return undefined;
+    }
+
+    console.log(dictionaries[1]);
+
+    tokens = text.split(/\s|(?=[\.\,])/);
+
+    tokens.forEach((token, i) => {
+      console.log("Cheching: ", token);
+      let found = false;
+      if (timeRegex.test(token)) {
+        //ks
+      } else {
+        dictionaries.forEach((dict) => {
+          if (dict[token]) {
+            translation += dict[token];
+            found = true;
+          }
+        });
+        if (!found) translation += token;
+      }
+      if (i < tokens.length - 1 && !/^\.|\,/.test(tokens[i + 1]))
+        translation += " ";
+    });
+
+    console.log(`-${translation}-`);
+    console.log(tokens);
+
+    return translation;
+  }
 }
 
 module.exports = Translator;
--- quality-assurance/american-british-translator/routes/api.js
+++ quality-assurance/american-british-translator/routes/api.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const { text } = require("body-parser");
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+    res.json({ text: text, translation: translator.translate(text, locale) });
+  });
 };
